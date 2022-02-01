577cdfaf9bd35e243578a825c713af790e6b1efa
Antoniadis Konstantinos <hello@pixelattack.gr>
2022-02-01 20:30:33 +0200

    initial configuration

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,12 @@
-module.exports = config => { 
+module.exports = (config) => {
     return {
-        
-    markdownTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
+        markdownTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
 
-      dir: {
-        input: 'src',
-        output: 'dist'
-      }
+        dir: {
+            input: "src",
+            output: "dist",
+        },
     };
-  };
+};
