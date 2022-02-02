3ceea312bca75f3bc9f06574eab6aca1b12938f8
Antoniadis Konstantinos <hello@pixelattack.gr>
2022-02-02 20:11:17 +0200

    feat: add distribution folder to release

--- release.config.js
+++ release.config.js
@@ -4,6 +4,16 @@ module.exports = {
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        "@semantic-release/github",
+        [
+            "@semantic-release/github",
+            {
+                assets: [
+                    {
+                        path: "dist.zip",
+                        label: "Distribution folder",
+                    },
+                ],
+            },
+        ],
     ],
 };
